317084bdeac395c02ba1f4955a29818d1049d2b1
Emmaexcellent <emmaexcellent030@gmail.com>
2024-12-27 18:34:45 +0100

    first commit

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 
 export const fetchCache = "force-no-store";
 
-import { Bot, webhookCallback } from "grammy";
+import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 
 const token = "7815956692:AAGNc1joV3IE4PpVTuKBt4hbfmaCJ2_7pQM";
 
@@ -10,8 +10,20 @@ if (!token)
   throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
 
 const bot = new Bot(token);
-bot.on("message:text", async (ctx) => {
-  await ctx.reply(ctx.message.text);
+
+bot.command("start", async (ctx) => {
+  const keyboard = new InlineKeyboard().url(
+    "Play Game",
+    "https://candycrushbot.vercel.app/" // Replace with your actual game URL
+  );
+
+  await ctx.reply("Welcome! Click the button below to start the game:", {
+    reply_markup: keyboard,
+  });
 });
+
+// bot.on("message:text", async (ctx) => {
+//   await ctx.reply(ctx.message.text);
+// });
 
 export const POST = webhookCallback(bot, "std/http");
